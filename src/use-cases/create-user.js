abde8b60b9aa6ed82dcf317c5d9183c5945e599c
igsalomao <igorsalomao13@gmail.com>
2024-07-12 18:46:14 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,32 @@
+// O use case é independente do banco de dados
+
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // TODO: verificar se o email ja esta em uso
+
+        // gerar ID do usuário
+        const userId = uuidv4()
+
+        //criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+        // inserir o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        // chamar o repositório
+        //TODO: injetar como dependência no constructor
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
