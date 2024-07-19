761dcb7af5b5bef6e91c98087570f2ca794010d3
igsalomao <igorsalomao13@gmail.com>
2024-07-19 14:22:28 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,44 @@
+import { bcrypt } from 'bcrypt'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        // 1. se o email estiver sendo atualizado, verificar se o email existe
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProviderEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProviderEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        // 2. se a senha estiver sendo atualizada, criptografar a nova senha
+        const user = { ...updateUserParams }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        // 3. chamar o repository para atualizar o usuário
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updateUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updateUser
+    }
+}
