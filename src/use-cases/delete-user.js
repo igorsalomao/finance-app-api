453d123482cfc5e4f6954c2c64bd406bcf31e73d
igsalomao <igorsalomao13@gmail.com>
2024-07-23 15:50:33 -0300

    feat: add DeleteUserUseCase

---
+++ src/use-cases/delete-user.js
@@ -0,0 +1,11 @@
+import { PostgresDeleteUserRepository } from '../repositories/postgres/index.js'
+
+export class DeleteUserUseCase {
+    async execute(userId) {
+        const postgresDeleteUserRepository = new PostgresDeleteUserRepository()
+
+        const deleteUser = postgresDeleteUserRepository.execute(userId)
+
+        return deleteUser
+    }
+}
