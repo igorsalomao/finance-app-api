b156b9957302f42c85324c3e1c8fc798bc05cbb6
igsalomao <igorsalomao13@gmail.com>
2024-08-13 17:23:45 -0300

    feat: add GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,37 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    serverError,
+    userNotFoundResponse,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+} from '../index.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const idIsValid = checkIfIdIsValid(userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ userId })
+
+            return ok(balance)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
