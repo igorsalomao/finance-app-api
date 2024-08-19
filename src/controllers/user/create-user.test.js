c61c30cb85642298af67869bd55a3eb8f9d7f874
igsalomao <igorsalomao13@gmail.com>
2024-08-19 17:17:53 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -84,4 +84,23 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(400)
         expect(result.body.message).toBe('E-mail is required.')
     })
+
+    it('should return 400 if email is not valid', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Igor',
+                last_name: 'Salomao',
+                email: 'igor',
+                password: '1234567',
+            },
+        }
+        // act
+        const result = await createUserController.execute(httpRequest)
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe('Please provide a valid e-mail.')
+    })
 })
