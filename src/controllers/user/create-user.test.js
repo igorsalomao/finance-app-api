d8d69eeb74979de1494fcf78ef28273d1f2479ea
igsalomao <igorsalomao13@gmail.com>
2024-08-19 17:05:30 -0300

    test: ensure 400 is returned if first_name isnt provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -28,4 +28,24 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                last_name: 'Salomao',
+                email: 'igor@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe('First name is required.')
+    })
 })
