875273085b144f59321df59907afed364d0a3cb7
igsalomao <igorsalomao13@gmail.com>
2024-08-20 17:47:23 -0300

    test: ensure GetUserBalanceController return 500 if GetUserBalanceUseCase throws

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import { DeleteUserController } from './delete-user'
 describe('Delete User Controller', () => {
     class DeleteUserUseCaseStub {
-        execute() {
+        async execute() {
             return {
                 id: faker.string.uuid(),
                 first_name: faker.person.firstName(),
@@ -50,7 +50,7 @@ describe('Delete User Controller', () => {
     it('should return 404 if user is not found', async () => {
         // arrange
         const { sut, deleteUserUseCase } = makeSut()
-        jest.spyOn(deleteUserUseCase, 'execute').mockReturnValueOnce(null)
+        jest.spyOn(deleteUserUseCase, 'execute').mockResolvedValue(null)
 
         // act
         const result = await sut.execute(httpRequest)
@@ -62,9 +62,9 @@ describe('Delete User Controller', () => {
     it('should return 500 if DeleteUserUseCase throws', async () => {
         // arrange
         const { sut, deleteUserUseCase } = makeSut()
-        jest.spyOn(deleteUserUseCase, 'execute').mockImplementationOnce(() => {
-            throw new Error()
-        })
+        jest.spyOn(deleteUserUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
         // act
         const result = await sut.execute(httpRequest)
 
