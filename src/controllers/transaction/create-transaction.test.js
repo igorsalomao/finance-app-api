5e9785bb32650c0fcb3912dab15108d3fddadd9e
igsalomao <igorsalomao13@gmail.com>
2024-08-22 18:20:05 -0300

    test: ensurre CreateTransactionController returns 400 when missing user_id

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -33,4 +33,20 @@ describe('CreateTransactionController', () => {
         // assert
         expect(result.statusCode).toBe(201)
     })
+
+    it('should return 400 when missing user_id', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                user_id: undefined,
+            },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
