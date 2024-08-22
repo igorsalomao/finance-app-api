abf2cddb248b1c57169a6c9d4b161c7c31311a88
igsalomao <igorsalomao13@gmail.com>
2024-08-22 16:18:00 -0300

    test: ensure UpdateUserController returns 400 if an invalid id is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -70,4 +70,20 @@ describe('UpdateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when an invalid id is provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            params: {
+                userId: 'invalid_id',
+            },
+            body: httpRequest.body,
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
