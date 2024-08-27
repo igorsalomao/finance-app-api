26f16ad61b790260402716d4a080084366c7ed27
igsalomao <igorsalomao13@gmail.com>
2024-08-27 17:51:33 -0300

    refactor: removo unused code

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,31 +1,4 @@
-import validator from 'validator'
-import { badRequest, notFound } from './index.js'
-
-export const checkIfAmountIsValid = (amount) => {
-    if (typeof amount !== 'number') {
-        return false
-    }
-
-    return validator.isCurrency(amount.toFixed(2), {
-        digits_after_decimal: [2],
-        allow_negatives: false,
-        decimal_separator: '.',
-    })
-}
-
-export const checkIfTypeIsValid = (type) => {
-    return ['EARNINGS', 'EXPENSE', 'INVESTMENT'].includes(type)
-}
-
-export const invalidAmountResponse = () =>
-    badRequest({
-        message: 'Amount must be a valid currency.',
-    })
-
-export const invalidTypeResponse = () =>
-    badRequest({
-        message: 'The type must be one of: EARNINGS, EXPENSE or INVESTMENT.',
-    })
+import { notFound } from './index.js'
 
 export const transactionNotFoundResponse = () => {
     return notFound({ message: 'Transaction not found.' })
