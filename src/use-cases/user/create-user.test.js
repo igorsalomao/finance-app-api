bfbc207b4e39bc67fa143b12657d4fd03a7b2b70
igsalomao <igorsalomao13@gmail.com>
2024-08-27 18:24:10 -0300

    test: ensure CreateUserUseCase create a user

---
+++ src/use-cases/user/create-user.test.js
@@ -0,0 +1,66 @@
+import { faker } from '@faker-js/faker'
+import { CreateUserUseCase } from './create-user'
+
+describe('CreateUserUseCase', () => {
+    class GetUserByEmailRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+
+    class CreateUserRepositoryStub {
+        async execute(user) {
+            return user
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async crypto() {
+            return 'hashed_password'
+        }
+    }
+
+    class IdGeneratorAdapterStub {
+        generate() {
+            return 'generated_id'
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+        const createUserRepository = new CreateUserRepositoryStub()
+        const passwordHasherAdapter = new PasswordHasherAdapterStub()
+        const idGeneratorAdapter = new IdGeneratorAdapterStub()
+
+        const sut = new CreateUserUseCase(
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHasherAdapter,
+            idGeneratorAdapter,
+        )
+
+        return {
+            sut,
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHasherAdapter,
+            idGeneratorAdapter,
+        }
+    }
+
+    it('should successfully create a user', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const createdUser = await sut.execute({
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({ length: 7 }),
+        })
+
+        // assert
+        expect(createdUser).toBeTruthy()
+    })
+})
