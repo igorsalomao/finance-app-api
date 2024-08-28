b82d6b69f65e14fd35a59bfe6af9609e6e8392ae
igsalomao <igorsalomao13@gmail.com>
2024-08-27 22:05:36 -0300

    test: ensure DeleteUserUseCase calls DeleteUserRepository with correct params

---
+++ src/use-cases/user/delete-user.test.js
@@ -0,0 +1,62 @@
+import { faker } from '@faker-js/faker'
+import { DeleteUserUseCase } from './delete-user'
+
+describe('DeleteUserUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    class DeleteUserRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const deleteUserRepository = new DeleteUserRepositoryStub()
+        const sut = new DeleteUserUseCase(deleteUserRepository)
+
+        return { sut, deleteUserRepository }
+    }
+
+    it('should successfully delete a user', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const deletedUser = await sut.execute(faker.string.uuid())
+
+        // assert
+        expect(deletedUser).toEqual(user)
+    })
+
+    it('should call DeleteUserRepository with correct params', async () => {
+        // arrange
+        const { sut, deleteUserRepository } = makeSut()
+        const executeSpy = jest.spyOn(deleteUserRepository, 'execute')
+        const userId = faker.string.uuid()
+
+        // act
+        await sut.execute(userId)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
+
+    it('should throw if DeleteUserRepository throws', async () => {
+        // arrange
+        const { sut, deleteUserRepository } = makeSut()
+        jest.spyOn(deleteUserRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(faker.string.uuid())
+
+        // assert
+        expect(promise).rejects.toThrow()
+    })
+})
